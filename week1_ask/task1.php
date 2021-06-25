f7be7808ff585ad83db2b2f1843067fb0f0ea642
adeboyejo akinbode <62593989+addybee@users.noreply.github.com>
2021-06-25 16:29:32 +0000

    ingnore week ask1

--- week1_ask/task1.php
+++
@@ -1,7 +0,0 @@
-    </head>
-    <body>
-        <?php
-        /*function that takes two arguments, start and end,
-         and returns an array containing all the numbers
-         from start up to end(including both start and end).*/
-            function rangeNum($start, $end){
