159fca68a99fe59be08eb83378fcb688ac55da92
adeboyejo akinbode <62593989+addybee@users.noreply.github.com>
2021-06-25 16:27:55 +0000

    edited the name of the folder "week1ask" to "week1task"

--- week1_ask/task1.php
+++ week1_ask/task1.php
@@ -1,8 +1,3 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset = "utf-8">
-        <title></title>
     </head>
     <body>
         <?php
@@ -10,19 +5,3 @@
          and returns an array containing all the numbers
          from start up to end(including both start and end).*/
             function rangeNum($start, $end){
-                $range=array();
-                //looping from start to end using forloop
-                for($i=$start; $i<=$end; $i++){
-                    array_push($range, $i);//pushing the numbers into the arra
-                }
-                return $range;
-            }
-            
-        /*function that takes an array of numbers and returns the sum of these numbers.*/
-            function add($numbers){
-                return array_sum($numbers);
-            }
-            
-        ?>
-    </body>
-</html>
